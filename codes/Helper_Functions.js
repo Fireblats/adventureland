bf862e6892568c457e23ce0115008f282fa9eaaf
Fireblats <MCampbell1092@gmail.com>
2023-06-24 17:18:22 -0500

    Changed To TS

---
+++ codes/Helper_Functions.js
@@ -0,0 +1,11 @@
+// A file containing common functions used throughout the project
+
+// Function: print(message)
+// Prints a message to the game console using the <code>safe_log</code> function
+//
+// Parameters:
+//     message - The message to print
+function print_custom(message){
+    safe_log(message);
+}
+safe_log('code loaded');
