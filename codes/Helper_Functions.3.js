82fa8b605702b21f75d19f2f923c90b7db1a295b
Fireblats <MCampbell1092@gmail.com>
2023-06-24 17:31:26 -0500

    Used Spaces Instead Of Tabs

--- codes/Helper_Functions.3.js
+++ codes/Helper_Functions.3.js
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 // A file containing common functions used throughout the project
 
 // Function: print(message)
@@ -5,29 +6,27 @@
 //
 // Parameters:
 //     message - The message to print
-async function print(message){
-	safe_log(message);
+async function print(message) {
+    safe_log(message);
 }
-
 
 /**
  * Returns the total quantity of a given item in the character's inventory.
  * @param {string} item_name - The name of the item to search for.
  * @returns {number} The total quantity of the item in the character's inventory.
  */
-function get_item_quantity(item_name){
-	let quantity = 0;
-	for (var i = 0; i < character.items.length; i++) {
-		if(character.items[i] == null){
-			break;
-		}
-		if(character.items[i].name == item_name){
-			quantity += character.items[i].q;
-		}
-	}
-	return quantity;
+function get_item_quantity(item_name) {
+    let quantity = 0;
+    for (let i = 0; i < character.items.length; i++) {
+        if (character.items[i] == null) {
+            break;
+        }
+        if (character.items[i].name === item_name) {
+            quantity += character.items[i].q;
+        }
+    }
+    return quantity;
 }
-
 
 /**
  * Checks if a given item exists in the character's inventory.
@@ -35,17 +34,17 @@ function get_item_quantity(item_name){
  * @returns {boolean} True if the item exists in the character's inventory, false otherwise.
  */
 function item_exists(item_name){
-	for (var i = 0; i < character.items.length; i++) {
-		if(character.items[i] == null){
-			break;
-		}
-		if(character.items[i].name == item_name){
-			return true;
-		}
-	}
-	return false;
-}
+    for (let i = 0; i < character.items.length; i++) {
+        if (character.items[i] == null) {
+            break;
+        }
+        if (character.items[i].name === item_name) {
+            return true;
+        }
+    }
 
+    return false;
+}
 
 /**
  * Sleeps for a given number of milliseconds.
@@ -53,5 +52,5 @@ function item_exists(item_name){
  * @returns {Promise<void>} A promise that resolves after the specified time has elapsed.
  */
 function sleep(ms) {
-	return new Promise(resolve => setTimeout(resolve, ms));
+    return new Promise((resolve) => setTimeout(resolve, ms));
 }
